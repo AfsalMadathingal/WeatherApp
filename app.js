3dc743d9fb02b6b1ccbefaf0f26e41981afcbf60
afsal <madathimgalafsal@gmail.com>
2024-02-28 21:45:44 +0530

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config();
-const mainRoute= require('./routes/mainRoute.js');
+const mainRoute= require('./Routes/mainRoute.js');
 const hbs = require('hbs');
 const path = require('path');
 
